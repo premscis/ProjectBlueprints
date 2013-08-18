a31630859d7ba35a4097da677e43e0b11ad9552c
Andreas Martin <andreas.martin@andreasmartin.ch>
2013-08-18 17:30:57 +0200

    Renamed package

--- Blueprint-JPAJavaApplication/src/main/java/ch/fhnw/mscbis/premscis/blueprint/jpajavaapplication/App.java
+++ Blueprint-JPAJavaApplication/src/main/java/ch/fhnw/mscbis/premscis/blueprint/jpajavaapplication/App.java
@@ -1,11 +1,11 @@
-package ch.fhnw.mscbis.premscis.blueprint.javaapplication;
+package ch.fhnw.mscbis.premscis.blueprint.jpajavaapplication;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import ch.fhnw.mscbis.premscis.blueprint.javaapplication.domain.Book;
+import ch.fhnw.mscbis.premscis.blueprint.jpajavaapplication.domain.Book;
 
 /**
  * Hello world!
--- Blueprint-JPAJavaApplication/src/main/java/ch/fhnw/mscbis/premscis/blueprint/jpajavaapplication/domain/Book.java
+++ Blueprint-JPAJavaApplication/src/main/java/ch/fhnw/mscbis/premscis/blueprint/jpajavaapplication/domain/Book.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ch.fhnw.mscbis.premscis.blueprint.javaapplication.domain;
+package ch.fhnw.mscbis.premscis.blueprint.jpajavaapplication.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
